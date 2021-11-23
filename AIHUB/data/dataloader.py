3433350e4d792e1e786153bdc4cd888bef6c52e3
Fr2zyRoom <Fr2zyRoom@github.com>
2021-11-23 06:28:47 +0000

    Update AIHUB code

--- AIHUB/data/dataloader.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import PIL.Image as Image
-
-def img_loader(img_path):
-    return np.expand_dims(np.array(Image.open(img_path)), axis=-1)
-def mask_loader(mask_path):
-    return np.expand_dims(np.where(np.array(Image.open(mask_path)),1,0), axis=-1).astype(np.uint8)
